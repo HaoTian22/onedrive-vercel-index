9850d59148f42a3df4088cf214790e219f307960
Hao_Tian22 <Hao_Tian22@outlook.com>
2022-02-12 11:35:42 +0800

    测试粤语

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'zh-CN', 'zh-TW', 'zh-yue']
+    locales: ['en', 'zh-CN', 'zh-TW', 'zh-Y']
   },
   localePath: path.resolve('public/locales'),
   reloadOnPrerender: process.env.NODE_ENV === 'development',
