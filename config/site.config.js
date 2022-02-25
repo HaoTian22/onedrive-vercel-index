c6d4c346dddbdeb409e4251ae9fbb3042e8ef52c
Hao_Tian22 <Hao_Tian22@outlook.com>
2022-02-25 22:16:06 +0800

    password

--- config/site.config.js
+++ config/site.config.js
@@ -40,7 +40,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/🔒私密文件夹', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: "mailto:hao_tian22@outlook.com",
